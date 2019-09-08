e9c3a6e036d31ec26778bb093e975657f2ec6a79
Eric Jivraj <Eric.Jivraj@pcmsgroup.com>
2019-09-08 16:54:48 +0100

    Initial commit

---
+++ src/test/java/DummyClassTest.java
@@ -0,0 +1,54 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class DummyClassTest
+{
+  private MyClass testClass;
+
+  public DummyClassTest()
+  {
+    testClass = new MyClass();
+  }
+
+  @Test
+  public void testReturnTrueFails()
+  {
+    assertTrue(testClass.returnFalse());
+  }
+
+  @Test
+  public void testReturnTruePasses()
+  {
+    assertTrue(testClass.returnTrue());
+  }
+
+  @Test
+  public void testReturnFalseFails()
+  {
+    assertFalse(testClass.returnTrue());
+  }
+
+  @Test
+  public void testReturnFalsePasses()
+  {
+    assertFalse(testClass.returnFalse());
+  }
+
+  class MyClass
+  {
+    public boolean returnTrue()
+    {
+      return true;
+    }
+
+    public boolean returnFalse()
+    {
+      return false;
+    }
+
+    public void doNothing()
+    {
+      // DO NOTHING
+    }
+  }
+}
