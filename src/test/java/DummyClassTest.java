f236f7e71b2705540f59f4c31082fec8db190d81
Eric Jivraj <ericjivraj@gmail.com>
2020-03-23 19:54:29 +0000

    Made dummy tests pass

--- src/test/java/DummyClassTest.java
+++ src/test/java/DummyClassTest.java
@@ -13,7 +13,7 @@ public class DummyClassTest
   @Test
   public void testReturnTrueFails()
   {
-    assertTrue(testClass.returnFalse());
+    assertTrue(testClass.returnTrue());
   }
 
   @Test
@@ -25,7 +25,7 @@ public class DummyClassTest
   @Test
   public void testReturnFalseFails()
   {
-    assertFalse(testClass.returnTrue());
+    assertFalse(testClass.returnFalse());
   }
 
   @Test
