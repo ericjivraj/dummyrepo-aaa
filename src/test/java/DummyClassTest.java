4caef465f122d31cbc57fd5e12aee72bbb5dc1bc
Eric Jivraj <ericjivraj@gmail.com>
2020-05-02 20:54:46 +0100

    Tests fail

--- src/test/java/DummyClassTest.java
+++ src/test/java/DummyClassTest.java
@@ -13,7 +13,7 @@ public class DummyClassTest
   @Test
   public void EricTrueFails()
   {
-    //assertTrue(testClass.returnFalse());
+    throw new NullPointerException();
   }
 
   @Test
@@ -25,7 +25,7 @@ public class DummyClassTest
   @Test
   public void EricFalseFails()
   {
-    //assertFalse(testClass.returnTrue());
+    assertFalse(testClass.returnTrue());
   }
 
   @Test
